27000de026426b95a2c5169824c0eb3413c9b053
leelic07 <lichengchao1@sina.cn>
2017-11-05 18:47:20 +0800

    外卖商城

---
+++ src/components/header/header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="header">
+    I am header
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+    export default {
+        data() {
+            return {}
+        }
+    }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
